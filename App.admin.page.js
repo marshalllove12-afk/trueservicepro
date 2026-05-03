d07de72f7264d6e6bae1776849908b6923c137a6
marshalllove12-afk <marshalllove12@gmail.com>
2026-05-03 13:52:25 -0400

    Delete App.admin.page.js

--- App.admin.page.js
+++
@@ -1,8 +0,0 @@
-export default function AdminPanel() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Admin Panel</h1>
-      <p>This is where admins will manage jobs, users, and disputes.</p>
-    </main>
-  );
-}
